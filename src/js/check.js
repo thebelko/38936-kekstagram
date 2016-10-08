7924ea19fa6fae6b9f8d4087cf04ba87cbd310c4
Olga Odnokolova <inc.belko@gmail.com>
2016-10-08 20:42:19 +0300

    правки по оформлению функций

--- src/js/check.js
+++ src/js/check.js
@@ -3,6 +3,14 @@ var getMessage = function (a, b) {
   var typeOfAIsMassive = Array.isArray(a);
   var typeOfBIsMassive = Array.isArray(b);
 
+  function getArraySum(array) {
+    var amount = 0;
+    for (var i = 0; i < array.length; i++) {
+      amount += array[i];
+    }
+    return amount;
+  }
+
   if (typeOfA === "boolean") {
     if (a === false) {
       return "Переданное GIF-изображение не анимировано";
@@ -12,33 +20,16 @@ var getMessage = function (a, b) {
   } else if (typeOfA === "number") {
       return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
   } else if (typeOfAIsMassive && !typeOfBIsMassive) {
-    function getArraySum(a) {
-      var amountOfRedPoints = 0;
-      for (var i = 0; i < a.length; i++) {
-        amountOfRedPoints += a[i];
-      }
-      return amountOfRedPoints;
-    }
     return "Количество красных точек во всех строчках изображения: " +  getArraySum(a);
   } else if (typeOfAIsMassive && typeOfBIsMassive) {
-    var subsidiaryArray = [];
     function getTwoArraysMultiplication(a, b) {
-      for (var i=0; i<a.length; i++) {
-        subsidiaryArray.push(a[i]*b[i]);
+      var subsidiaryArray = [];
+      for (var i=0; i < a.length; i++) {
+        subsidiaryArray.push(a[i] * b[i]);
       }
       return subsidiaryArray;
     }
-
-    getTwoArraysMultiplication(a, b);
-
-    function getArraySum(subsidiaryArray) {
-      var artifactsSquare = 0;
-      for (var i = 0; i < subsidiaryArray.length; i++) {
-        artifactsSquare += subsidiaryArray[i];
-      }
-      return artifactsSquare;
-    }
-    return "Общая площадь артефактов сжатия: " + getArraySum(subsidiaryArray) + " пикселей";
+    return "Общая площадь артефактов сжатия: " + getArraySum(getTwoArraysMultiplication(a, b)) + " пикселей";
   } else {
     return "Переданы некорректные данные";
   }
